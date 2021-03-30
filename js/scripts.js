d438ffb9860c6aa9c2dfa3ffdf8868c906d7b04d
David Miller <hello@davidmiller.io>
2021-03-30 19:48:55 -0400

    theme restructure
    removed gulp dependency in favor of npm scripts, update to Bootstrap 4.6, reorganize project with pug and scss

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Business Casual v6.0.0 (https://startbootstrap.com/theme/business-casual)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+*/
+// Highlights current date on contact page
+$('.list-hours li').eq(new Date().getDay()).addClass('today');
