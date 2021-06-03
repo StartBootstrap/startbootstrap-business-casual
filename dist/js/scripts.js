4c847261dccf26610953f75d7362406ca99b5e5e
David Miller <hello@davidmiller.io>
2021-06-03 17:09:02 -0400

    update dependencies

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Business Casual v7.0.0 (https://startbootstrap.com/theme/business-casual)
+* Start Bootstrap - Business Casual v7.0.1 (https://startbootstrap.com/theme/business-casual)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
 */
