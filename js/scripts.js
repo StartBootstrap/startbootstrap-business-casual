d59ad79cc1632b1b75d2a1bdfecd5213b104e870
David Miller <hello@davidmiller.io>
2022-03-22 16:34:56 -0400

    dependencies update

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Business Casual v7.0.6 (https://startbootstrap.com/theme/business-casual)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
 */
 // Highlights current date on contact page
