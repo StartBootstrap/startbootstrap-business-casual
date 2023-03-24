b6f928934386a54e08c2e7f40ebd9e4d7510bba4
David Miller <hello@davidmiller.io>
2023-03-24 18:36:33 -0400

    Bootstrap 5.2.3 dependency update
    Removed font awesome as it is not being used anymore and updated dependencies

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Business Casual v7.0.8 (https://startbootstrap.com/theme/business-casual)
-* Copyright 2013-2022 Start Bootstrap
+* Start Bootstrap - Business Casual v7.0.9 (https://startbootstrap.com/theme/business-casual)
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
 */
 // Highlights current date on contact page
